f61060a97c5b6c44cf92556b46a7d0874f7a0ee9
Jain, Mayur Mayur.Jain@parc.com <Mayur.Jain@parc.com>
2015-11-09 14:22:15 -0800

    readme update

--- README.txt
+++ README.txt
@@ -4,7 +4,7 @@ CMPE 275 Lab - 2
 
 Team 22 :
 
-Member1 : Mayur Jain | 009991059 | mayurj07@gmail.com
+Member1 : Mayur Jain     | 009991059 | mayurj07@gmail.com
 Member2 : Akanksha Singh | 010030839 | akanksha.singh@sjsu.edu,
 Member3 : Harkirat Singh | 010027823 | harkirat.singh@sjsu.edu
 
@@ -21,20 +21,36 @@ How teh application works:
               Delete Person
               Update Person Info
               Get Person Info
+
 2) All the APIs in PersonController call menthods of PersonService, Which further calls methods of PersonDAO.
+
 3) OrganizationController has  all the APIs related to organization data. It contains following APIs -
               Create Organization
               Delete Organization
               Update Organization Info
               Get Organization Info
+
 4)All the methods in OrganizationController call methods of OrganizationService, which further calls methods of OrganizationDAO.
+
 5)FriendshipController has  all the APIs related to organization data. It contains following APIs -
               Add Friendship
               Remove Friendship
+
  6)All the methods in FriendshipController call methods of FriendshipService, which further calls methods of FriendshipDAO.
 
 -------package: edu.sjsu.cmpe275.lab2;
 
 
+Note: Mysql dump file also included (filename:    SQLscript.sql)
+
 
 -pom.xml: All the maven dependencies go inside this file.
+
+
+Maven must be installed.
+
+How to Run:
+
+1. $ mvn clean install
+
+2. Run Tomcat server.
---
+++ SQLScript.sql
@@ -0,0 +1,91 @@
+CREATE DATABASE  IF NOT EXISTS `cmpelab2` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `cmpelab2`;
+-- MySQL dump 10.13  Distrib 5.7.9, for osx10.9 (x86_64)
+--
+-- Host: 127.0.0.1    Database: cmpelab2
+-- ------------------------------------------------------
+-- Server version	5.7.9
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `friendship`
+--
+
+DROP TABLE IF EXISTS `friendship`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `friendship` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `person1` int(11) DEFAULT NULL,
+  `person2` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `person1_idx` (`person1`),
+  KEY `person2_idx` (`person2`),
+  CONSTRAINT `person1` FOREIGN KEY (`person1`) REFERENCES `person` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `person2` FOREIGN KEY (`person2`) REFERENCES `person` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `organization`
+--
+
+DROP TABLE IF EXISTS `organization`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `organization` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `description` varchar(100) DEFAULT NULL,
+  `street` varchar(45) DEFAULT NULL,
+  `city` varchar(45) DEFAULT NULL,
+  `state` varchar(45) DEFAULT NULL,
+  `zip` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `person`
+--
+
+DROP TABLE IF EXISTS `person`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `person` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(45) NOT NULL,
+  `lastname` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `description` varchar(45) DEFAULT NULL,
+  `street` varchar(45) DEFAULT NULL,
+  `city` varchar(45) DEFAULT NULL,
+  `state` varchar(45) DEFAULT NULL,
+  `zip` varchar(45) DEFAULT NULL,
+  `orgid` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `orgid_idx` (`orgid`),
+  CONSTRAINT `orgid` FOREIGN KEY (`orgid`) REFERENCES `organization` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-11-08 23:52:38
